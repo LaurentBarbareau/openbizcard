10c16084e6dc84ac589ddd8f06efb8910508bbbb
hideoaki <hideoaki@213e368e-d0c6-11de-9889-db2f8e67c15c>
2010-01-31 16:47:42 +0000

    Fix Training

--- trunk/DHLSvn/src/com/tssoftgroup/tmobile/model/Question.java
+++ trunk/DHLSvn/src/com/tssoftgroup/tmobile/model/Question.java
@@ -6,5 +6,5 @@ public class Question {
 	public int numQuestion = 0;
 	public String question = "";
 	public Vector choices = new Vector();
-	public String answer = "a";
+	public String answer = "";
 }
