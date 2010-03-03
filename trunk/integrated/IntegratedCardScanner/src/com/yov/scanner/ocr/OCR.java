15068588022d7c6ecc225cf67fefb59f35a29b8f
hideoaki <hideoaki@213e368e-d0c6-11de-9889-db2f8e67c15c>
2010-03-03 06:25:57 +0000

    Add Java code to support thai

--- trunk/integrated/IntegratedCardScanner/src/com/yov/scanner/ocr/OCR.java
+++ trunk/integrated/IntegratedCardScanner/src/com/yov/scanner/ocr/OCR.java
@@ -43,7 +43,21 @@ public class OCR {
 
         cmd.add(LANG_OPTION);
         cmd.add(lang);
-            
+        // Command for thai
+         List<String> cmdT = new ArrayList<String>();
+        System.out.println("Tessaract path " +tessPath + File.separator+"tesseract" );
+        if(File.separator.equals("/")){
+            cmdT.add("tesseract");
+        }else{
+             cmdT.add(tessPath + File.separator+"tesseract");
+        }
+        cmdT.add(""); // placeholder for inputfile
+        cmdT.add(outputFile.getName());
+        System.out.println("outputFile.getName " + outputFile.getName() );
+
+        cmdT.add(LANG_OPTION);
+        cmdT.add("tha");
+        //
         ProcessBuilder pb = new ProcessBuilder();
         pb.directory(imageFile.getParentFile());
             
@@ -62,7 +76,7 @@ public class OCR {
             System.out.println("Exit value = " + w);
             
             // delete temp working files
-            tempImage.delete();
+            //tempImage.delete();
             
             if (w == 0) {
                 BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(outputFile.getAbsolutePath() + ".txt"), "UTF-8"));
@@ -93,7 +107,51 @@ public class OCR {
                 }
                 throw new RuntimeException(msg);
             }
-            
+            // Thai
+             System.out.println("input file:" + tempImage.getName());
+//            cmd.set(1, tempImage.getName());
+           cmdT.set(1, tempImage.getName());
+           pb.command(cmdT);
+            pb.redirectErrorStream(true);
+            Process process2 = pb.start();
+//            Process process = Runtime.getRuntime().exec(cmd.toArray(new String[0]));
+
+            int w2 = process2.waitFor();
+            System.out.println("Exit value = " + w);
+
+            // delete temp working files
+            tempImage.delete();
+
+            if (w2 == 0) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(outputFile.getAbsolutePath() + ".txt"), "UTF-8"));
+
+                String str;
+
+                while ((str = in.readLine()) != null) {
+                    strB.append(str).append(EOL);
+                }
+                in.close();
+            } else {
+                String msg;
+                switch (w2) {
+                    case 1:
+                        msg = "Errors accessing files. There may be spaces in your image's filename.";
+                        break;
+                    case 29:
+                        msg = "Cannot recognize the image or its selected region.";
+                        break;
+                    case 31:
+                        msg = "Unsupported image format.";
+                        break;
+                    default:
+                        msg = "Errors occurred.";
+                }
+                for (File image : tempImages) {
+                    image.delete();
+                }
+                throw new RuntimeException(msg);
+            }
+
         }
         new File(outputFile.getAbsolutePath() + ".txt").delete();
         return strB.toString();
