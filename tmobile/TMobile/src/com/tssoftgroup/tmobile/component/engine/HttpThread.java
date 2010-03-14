248c8d8a0c57a5d23e1d2ba270d72c584c97b2be
hideoaki <hideoaki@213e368e-d0c6-11de-9889-db2f8e67c15c>
2010-03-14 17:55:47 +0000

    Add HTTP Retry 5 times

--- tmobile/TMobile/src/com/tssoftgroup/tmobile/component/engine/HttpThread.java
+++ tmobile/TMobile/src/com/tssoftgroup/tmobile/component/engine/HttpThread.java
@@ -4,15 +4,15 @@ import java.util.Vector;
 
 import com.tssoftgroup.tmobile.utils.Const;
 
-
 public class HttpThread extends Thread {
 
 	Vector bodies = new Vector();
 	Vector urls = new Vector();
 	Vector modes = new Vector();
 	boolean mTrucking = true;
-	
-	HTTPHandler handler ;
+
+	HTTPHandler handler;
+
 	public HttpThread(HTTPHandler handler) {
 		this.handler = handler;
 	}
@@ -27,19 +27,19 @@ public class HttpThread extends Thread {
 	public synchronized void run() {
 		while (mTrucking) {
 			if (urls.size() > 0) {
-//				try {//TODO
-//					Thread.sleep(1000);
-//				} catch (InterruptedException e) {
-//					e.printStackTrace();
-//				}
-//				boolean excep = false;
+				// try {//TODO
+				// Thread.sleep(1000);
+				// } catch (InterruptedException e) {
+				// e.printStackTrace();
+				// }
+				// boolean excep = false;
 				String url = (String) urls.elementAt(0);
 				String body = (String) bodies.elementAt(0);
 				int mode = ((Integer) modes.elementAt(0)).intValue();
 				System.out.println("-----");
-				System.out.println("url "+ url);
-				System.out.println("body "+ body);
-				
+				System.out.println("url " + url);
+				System.out.println("body " + body);
+
 				urls.removeElementAt(0);
 				bodies.removeElementAt(0);
 				modes.removeElementAt(0);
@@ -48,32 +48,31 @@ public class HttpThread extends Thread {
 					res = getContent(url, body, true);
 					System.out.println("myresponse " + res);
 				}
-//				catch (SecurityException e) {
-////					excep = true;
-//				}
+				// catch (SecurityException e) {
+				// // excep = true;
+				// }
 				catch (Exception e) {
 					e.printStackTrace();
 					for (int i = 0; i < Const.NUM_RETRY_HTTP; i++) {
 						try {
 							res = getContent(url, body, true);
-							
+
 							System.out.println("myresponse " + res);
-						}catch (Exception ex) {
+						} catch (Exception ex) {
 							ex.printStackTrace();
-//							System.out.println("\n---------\n error httpcon.close() "+e.getMessage()+e.toString()+"\n");
+							// System.out.println("\n---------\n error httpcon.close() "+e.getMessage()+e.toString()+"\n");
 						}
 						break;
 					}
-//					System.out.println("\n---------\n error httpcon.close() "+e.getMessage()+e.toString()+"\n");
+					// System.out.println("\n---------\n error httpcon.close() "+e.getMessage()+e.toString()+"\n");
 				}
 				try {
 					handler.finishCallback(res, mode);
+				} catch (Exception ex) {
+					ex.printStackTrace();
+					// System.out.println("\n---------\n error httpcon.close() "+e.getMessage()+e.toString()+"\n");
 				}
-					catch (Exception ex) {
-						ex.printStackTrace();
-//						System.out.println("\n---------\n error httpcon.close() "+e.getMessage()+e.toString()+"\n");
-					}
-				
+
 			} else {
 				try {
 					waiting = true;
@@ -85,21 +84,25 @@ public class HttpThread extends Thread {
 			} // if
 		}
 	}
+
 	boolean waiting = false;
+
 	public void go() {
-//		System.out.println("Start Go");
-		if(waiting){
-//			System.out.println("In Go");
+		// System.out.println("Start Go");
+		if (waiting) {
+			// System.out.println("In Go");
 			mynotify();
 			waiting = false;
 		}
-//		System.out.println("End Go");
+		// System.out.println("End Go");
 	}
-	private synchronized void mynotify(){
+
+	private synchronized void mynotify() {
 		notify();
 	}
 
-	public String getContent(String url, String body, boolean cancelable) throws Exception {
+	public String getContent(String url, String body, boolean cancelable)
+			throws Exception {
 		String response = "";
 		try {
 
